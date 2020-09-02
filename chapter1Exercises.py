ada76b6271aaeb17ff8022ca38c05399c9ab3736
peterliu502 <798551899@qq.com>
2020-09-03 01:38:11 +0200

    finish exercises in chapter1

---
+++ chapter1Exercises.py
@@ -0,0 +1,20 @@
+# Exercise 1.2.1
+seconds = 42 * 60 + 42
+print(seconds)
+
+# Exercise 1.2.2
+miles = 10 / 1.61
+print(miles)
+
+# Exercise 1.2.3
+# per minutes
+speed_in_minutes = miles / (42 + 42 / 60)
+print(speed_in_minutes)
+
+# per seconds
+speed_in_seconds = miles / seconds
+print(speed_in_seconds)
+
+# per hour
+speed_in_hour = miles / ((42 + 42 / 60) / 60)
+print(speed_in_hour)
